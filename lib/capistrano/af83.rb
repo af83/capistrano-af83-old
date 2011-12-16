710394e465455463b2e8bbec27b6ba38a036aad4
Bruno Michel <bmichel@menfin.info>
2011-12-16 09:38:27 +0100

    Fix rails s thin (Capistrano::Configuration is not defined)

--- lib/capistrano/af83.rb
+++ lib/capistrano/af83.rb
@@ -1,7 +1,10 @@
 # This is our default stack
 
 require "capistrano"
-Capistrano::Configuration.instance.load_paths << File.dirname(__FILE__)
-Capistrano::Configuration.instance.load "af83/default"
-Capistrano::Configuration.instance.load "af83/environments"
-require "capistrano/af83/extensions"
+
+if defined?(Capistrano::Configuration)
+  Capistrano::Configuration.instance.load_paths << File.dirname(__FILE__)
+  Capistrano::Configuration.instance.load "af83/default"
+  Capistrano::Configuration.instance.load "af83/environments"
+  require "capistrano/af83/extensions"
+end
