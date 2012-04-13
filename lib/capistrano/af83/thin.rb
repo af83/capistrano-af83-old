8cc45c1fc99956a47e1474999fb36b74894ee63b
Laurent Arnoud <laurent@spkdev.net>
2012-04-13 11:41:38 +0200

    Add description for thin start,stop,restart.

--- lib/capistrano/af83/thin.rb
+++ lib/capistrano/af83/thin.rb
@@ -3,14 +3,17 @@
 depend :remote, :command, "bundle"
 
 namespace :deploy do
+  desc 'Start thin'
   task :start, :roles => :app do
     run "cd #{current_path} && #{bundle_cmd} exec thin start -C #{shared_path}/thin.yml"
   end
 
+  desc 'Stop thin'
   task :stop, :rules => :app do
     run "cd #{current_path} && #{bundle_cmd} exec thin stop -C #{shared_path}/thin.yml"
   end
 
+  desc 'Restart thin'
   task :restart, :roles => :app, :except => { :no_release => true }  do
     run "cd #{current_path} && #{bundle_cmd} exec thin restart -C #{shared_path}/thin.yml"
   end
