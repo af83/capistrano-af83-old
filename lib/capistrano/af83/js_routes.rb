bdc074f90dd0a42e54213ef6100c3bd05399e767
Laurent Arnoud <larnoud@af83.com>
2013-02-12 14:41:44 +0100

    Js_routes: added :bower_install task

--- lib/capistrano/af83/js_routes.rb
+++ lib/capistrano/af83/js_routes.rb
@@ -5,6 +5,11 @@ namespace :js do
   task :create_routes do
     run "cd #{release_path} && #{bundle_cmd} exec rake js:routes"
   end
+
+  desc "Download JS/CSS assets using bower"
+  task :bower_install do
+    run "cd #{release_path} && bower install"
+  end
 end
 
 after "deploy:update", "js:create_routes"
