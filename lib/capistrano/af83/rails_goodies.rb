453f53ca6bdd2b3d00932d80bdd0fa7622f92b1d
Arnaud Berthomier <oz@cyprio.net>
2013-03-14 21:23:52 -0600

    Some goodies for Rails apps
      - rails:log     Tails Rails log file
      - rails:console Opens a Rails console

---
+++ lib/capistrano/af83/rails_goodies.rb
@@ -0,0 +1,24 @@
+require 'readline'
+
+namespace :rails do
+  desc "Tail Rails logs"
+  task :log do
+    stream "tail -f #{shared_path}/log/#{rails_env}.log"
+  end
+
+  desc "Open a Rails console"
+  task :console do
+    input = ''
+    run "cd #{current_path} && #{bundle_cmd} exec rails console" do |chan, stream, data|
+      next if data.chomp == input.chomp || data.chomp == ''
+      print data
+      next unless data.chomp =~ />\s+$/ # Bad prompt detection
+
+      # Readline's quite tolerant, but most control codes (Ctrl-u, Ctrl-l, ...)
+      # will screw up the process' stream.
+      input = Readline.readline('', true)
+      break if input.nil? # EOF
+      chan.send_data "#{input}\n"
+    end
+  end
+end
