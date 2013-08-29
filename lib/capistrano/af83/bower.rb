fdcdf259099d640727844aca0428a939a68dd72e
Laurent Arnoud <laurent@spkdev.net>
2013-08-29 12:12:00 +0200

    Bower: improve install and added cache clean task
    This fix dependencies problems
    
    Signed-off-by: Laurent Arnoud <laurent.arnoud@af83.com>
    Signed-off-by: Gilles Robit <gilles.robit@af83.com>

--- lib/capistrano/af83/bower.rb
+++ lib/capistrano/af83/bower.rb
@@ -1,7 +1,17 @@
 # encoding: UTF-8
 namespace :bower  do
-  desc "Download JS/CSS assets using bower"
+  desc "Install JS/CSS assets using bower"
   task :install do
-    run "cd #{release_path} && bower install"
+    run "cd #{release_path} && bower install --production"
+  end
+
+  desc 'Force install and resolve dependencies'
+  task :force_install do
+    run "cd #{release_path} && bower install --production --force-latest"
+  end
+
+  desc 'Cache clean'
+  task :cache_clean do
+    run "cd #{release_path} && bower cache clean"
   end
 end
