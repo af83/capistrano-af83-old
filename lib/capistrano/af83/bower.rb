5507adb6bd4038dee5704ee0a279b4beeb505038
Laurent Arnoud <laurent@spkdev.net>
2013-10-24 16:46:15 +0300

    Bower: added check_install task

--- lib/capistrano/af83/bower.rb
+++ lib/capistrano/af83/bower.rb
@@ -1,5 +1,10 @@
 # encoding: UTF-8
 namespace :bower  do
+  desc 'Install bower executable on the server'
+  task :check_install do
+    run 'command -v bower >/dev/null 2>&1 || npm install bower -g'
+  end
+
   desc "Install JS/CSS assets using bower"
   task :install do
     run "cd #{release_path} && bower --no-color install --production"
@@ -15,3 +20,5 @@ namespace :bower  do
     run "cd #{release_path} && bower --no-color cache clean"
   end
 end
+
+before 'deploy:update_code', 'bower:check_install'
--- lib/capistrano/af83/bundler.rb
+++ lib/capistrano/af83/bundler.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 
 namespace :bundler do
+  # XXX maybe rename this to be equal with bower:check_install
   task :install do
     run 'gem query --local | grep bundler || gem install bundler'
   end
