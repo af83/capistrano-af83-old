492c826267bddfc0d87732877001e3d93ae29bb6
klacointe <kevinlacointe@gmail.com>
2014-01-23 14:29:54 +0100

    Upload ruby-env.

---
+++ lib/capistrano/af83/ruby_env.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+namespace :ruby_env do
+  desc "Copy ruby-env"
+  task :copy do
+    src = "admin/ruby-env/#{rails_env}"
+    if File.exists? src
+      upload src, "ruby-env", via: :scp
+    else
+      logger.info "ruby-env for #{rails_env} doesn't exists."
+    end
+  end
+end
+before 'deploy', 'ruby_env:copy'
