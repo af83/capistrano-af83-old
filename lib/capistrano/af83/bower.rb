5b0cd6dbb4d593daacafa48d0306f3a4e7ac2301
klacointe <kevinlacointe@gmail.com>
2014-03-20 10:13:03 +0100

    [Bower] Add bower update task.
    Also pass --config.interactive=false argument.

--- lib/capistrano/af83/bower.rb
+++ lib/capistrano/af83/bower.rb
@@ -7,12 +7,17 @@ namespace :bower  do
 
   desc "Install JS/CSS assets using bower"
   task :install do
-    run "cd #{release_path} && bower --no-color install --production"
+    run "cd #{release_path} && bower --no-color install --production --config.interactive=false"
+  end
+
+  desc "Install JS/CSS assets using bower (updating versions if needed)"
+  task :update do
+    run "cd #{release_path} && bower --no-color update --production --config.interactive=false"
   end
 
   desc 'Force install and resolve dependencies'
   task :force_install do
-    run "cd #{release_path} && bower --no-color install --production --force-latest"
+    run "cd #{release_path} && bower --no-color install --production --force-latest --config.interactive=false"
   end
 
   desc 'Cache clean'
